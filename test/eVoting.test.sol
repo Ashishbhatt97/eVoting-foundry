dd4889c40acf306ccfc66490d0179a57d18d48ac
Ashishbhatt97 <ashishbhatt0197@gmail.com>
2024-06-14 14:53:36 +0530

    test ownership test done

--- test/eVoting.test.sol
+++ test/eVoting.test.sol
@@ -34,9 +34,30 @@ contract eVotingTest is Test {
         voting.addCandidate("Bob", "Party B");
     }
 
-
-
     function testVote() public {
+        voting.addCandidate("ashish", "BJP");
+        voting.addCandidate("ayush", "INC");
 
+        assertEq(voting.votingCounts(), 0); //Checking initial voting counts in polling
+        assertEq(voting.votes(1), 0); // checking 1st candidate's votes
+        assertEq(voting.votes(2), 0); //checking 2nd candidate's votes
+
+        //vote for candidate 1
+        voting.vote(1);
+        assertEq(voting.votes(1), 1);
+        assertEq(voting.votingCounts(), 1);
+
+        vm.expectRevert("you have already voted");
+        voting.vote(2);
+    }
+
+
+
+    function testOwnershipOnlyOwnerCanAddCandidates() public {
+    address nonOwner = address(0x05584EFd);
+    vm.prank(nonOwner);
+    vm.expectRevert("Only Owner can make changes");
+    voting.addCandidate("Ashish Bhatt", "BJP");
+    
     }
 }
