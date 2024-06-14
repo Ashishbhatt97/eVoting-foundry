5608e0d9875ee28983177beb2b3c36bb3f73478a
Ashishbhatt97 <ashishbhatt0197@gmail.com>
2024-06-14 14:42:16 +0530

    test Add candidate done

--- test/eVoting.test.sol
+++ test/eVoting.test.sol
@@ -6,5 +6,37 @@ import "forge-std/Test.sol";
 import "../src/eVoting.sol";
 
 contract eVotingTest is Test {
+    eVoting voting;
+    address owner = address(this);
 
+    function setUp() public {
+        voting = new eVoting();
+    }
+
+    function testAddCandidate() public {
+        assertEq(voting.candidateCount(), 0); //Initial candidate Count should be 0
+
+        voting.addCandidate("Ashish Bhatt", "BJP"); //Adding candidate
+        assertEq(voting.candidateCount(), 1);
+
+        (string memory name, uint256 number, string memory _party) = voting
+            .activeCandidates(0); // checking the added candidate details
+
+        assertEq(name, "Ashish Bhatt");
+        assertEq(_party, "BJP");
+        assertEq(number, 1);
+
+        //adding a candidate from a non owner address should be revert
+
+        address nonOwner = address(0x000000458);
+        vm.prank(nonOwner);
+        vm.expectRevert("Only Owner can make changes");
+        voting.addCandidate("Bob", "Party B");
+    }
+
+
+
+    function testVote() public {
+
+    }
 }
